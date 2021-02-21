370f0a2bfc5c77986ecea9aaa129c4860387787a
Arthur Burle <arthurburle@gmail.com>
2021-02-21 16:51:51 -0300

    repushing the whole folder

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
